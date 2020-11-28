f7fcac90334c4c0a365cbfeb43fe39b9f3af7b62
Vincent He <vincent0710@Outlook.com>
2020-11-28 14:08:29 -0500

    Added database constants

--- API/constants.py
+++ API/constants.py
@@ -1,3 +1,11 @@
 import os
+import mysql.connector
+from CRED import PASSWORD
 
 IMAGES_DIR = os.path.join(os.curdir, "images")
+db = mysql.connector.connect(
+    user='root',
+    password=PASSWORD,
+    host='localhost',
+)
+cursor = db.cursor()
