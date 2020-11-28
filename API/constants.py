cd543811cf3ed2be9e3e3d81347e3b35cc479d75
Vincent He <vincent0710@Outlook.com>
2020-11-28 15:00:19 -0500

    Added other directory

--- API/constants.py
+++ API/constants.py
@@ -2,7 +2,8 @@ import os
 import mysql.connector
 from CRED import PASSWORD
 
-IMAGES_DIR = os.path.join(os.curdir, "images")
+IMAGES_DIR = os.path.join(os.curdir, "Data", "images")
+PLACEHOLDING_DATA_DIR = os.path.join(os.curdir, "Data", "PlaceHoldingData")
 db = mysql.connector.connect(
     user='root',
     password=PASSWORD,
