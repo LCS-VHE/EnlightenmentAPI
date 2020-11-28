09ea936fdc470230712c00471d84c39f3803879c
Vincent He <vincent0710@Outlook.com>
2020-11-28 15:29:46 -0500

    Setup the basic structure of the database

---
+++ API/database.py
@@ -0,0 +1,16 @@
+from constants import cursor
+import mysql.connector
+
+Q1 = "CREATE DATABASE EnlightenmentApi" # Create a database
+Q2 = "CREATE TABLE Posts (postId INT PRIMARY KEY AUTO_INCREMENT, accountId INT, timestamp INT, madeWith VARCHAR(100), fileLocation VARCHAR(300), title VARCHAR(300), captions VARCHAR(500), likes INT DEFAULT 0, isPrivate BOOL)" # Table one
+Q3 = "CREATE TABLE Tags (postId INT, FOREIGN KEY(postId) REFERENCES Posts(postId), tag1 VARCHAR(100), tag2 VARCHAR(100), tag3 VARCHAR(100 ))" # Table two
+cursor.execute(Q2)
+cursor.execute(Q3)
+
+cursor.execute("DESCRIBE Posts")
+for x in cursor:
+    print(x)
+
+cursor.execute("DESCRIBE Tags")
+for x in cursor:
+    print(x)
