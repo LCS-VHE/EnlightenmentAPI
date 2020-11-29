be8bf2fbd5e7927d5e00ad40fffb0902d4771988
Vincent He <vincent0710@Outlook.com>
2020-11-28 20:13:08 -0500

    Change in image directory

--- API/constants.py
+++ API/constants.py
@@ -2,7 +2,7 @@ import os
 import mysql.connector
 from CRED import PASSWORD
 
-IMAGES_DIR = os.path.join(os.curdir, "Data", "images")
+IMAGES_DIR = os.path.join(os.curdir, "Data", "Images")
 PLACEHOLDING_DATA_DIR = os.path.join(os.curdir, "Data", "PlaceHoldingData")
 db = mysql.connector.connect(
     user='root',
