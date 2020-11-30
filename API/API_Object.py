b7b5cd61bf4ad4699daecf96ebca7860a1e7e3de
Vincent He <vincent0710@Outlook.com>
2020-11-30 12:58:45 -0500

    Return json file success

--- API/API_Object.py
+++ API/API_Object.py
@@ -6,6 +6,8 @@ from constants import db, cursor # Accessing database
 
 class GetPostsFromUser(Resource): # Return all the posts from a given user
     num_amount_posts = 10
+    def get_sql_command(self, id):
+        return f"SELECT postId, accountId, timestamp, madeWith, filelocation, title, captions, likes, isPrivate FROM Posts WHERE accountId={id}"
 
     def get(self, id):
         """
@@ -13,7 +15,21 @@ class GetPostsFromUser(Resource): # Return all the posts from a given user
         :param id: uesr id that referes to the database
         :return: Json object that has a list of posts
         """
-        return {"Success" : id}
+        cursor.execute(self.get_sql_command(id))
+        posts = []
+        for element in cursor:
+            posts.append({
+                "postId" : element[0],
+                "accountId": element[1],
+                "timestamp": element[2],
+                "madeWith": element[3],
+                "filelocation": element[4], # This would be get url link in the future
+                "title": element[5],
+                "likes": element[6],
+                "isPrivate": element[7],
+            })
+
+        return {"Posts" : posts}
 
 class GetPosts(Resource):
     def get(self):
